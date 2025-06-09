4514b5ccea217865f099fcb2ae2ed93c85c51983
VISHWAJEET <87874967+vishwajeet2022@users.noreply.github.com>
2025-06-09 17:48:29 +0530

    Add files via upload

---
+++ src/main/java/com/tms/filter/JWTGenerationFilter.java
@@ -0,0 +1,44 @@
+package com.tms.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.tms.apputil.TMSConstants;
+import com.tms.config.JWTUtility;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTGenerationFilter extends OncePerRequestFilter {
+
+	private JWTUtility jwtUtility;
+
+	public JWTGenerationFilter(JWTUtility jwtUtility) {
+		super();
+		this.jwtUtility = jwtUtility;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+		String jwt = null;
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			jwt = jwtUtility.doGenerateToken(authentication.getName(), authentication.getAuthorities());
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		response.addHeader(TMSConstants.AUTHORIZATION_HEADER, TMSConstants.BEARER + jwt);
+		filterChain.doFilter(request, response);
+
+	}
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		return !request.getServletPath().equals("/api/auth/login");
+	}
+}
